63f91cf454388d83787bd105f910900213b23b1a
michal7kw <michal.k7w@gmail.com>
2025-02-10 11:31:24 +0100

    sync

--- TCGA/run_High_SRRM3_PSI_Analysis.sh
+++ TCGA/run_High_SRRM3_PSI_Analysis.sh
@@ -69,8 +69,33 @@ options(error = function() traceback(2))
 options(future.globals.maxSize = 8000 * 1024^2)
 options(mc.cores = parallel::detectCores() - 1)
 
-# Source analysis script
-source("High_SRRM3_PSI_Analysis.R")
+# Load required libraries
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(survival)
+  library(survminer)
+  library(recount3)
+  library(biomaRt)
+  library(parallel)
+  library(BiocParallel)
+  library(TCGAbiolinks)
+  library(SummarizedExperiment)
+  library(tidyverse)
+  library(DESeq2)
+  library(httr)
+  library(retry)
+  library(futile.logger)
+  library(GenomicFeatures)
+  library(rtracklayer)
+  library(matrixStats)
+  library(sparseMatrixStats)
+  library(viridis)
+  library(data.table)
+})
+
+# Source analysis scripts
+source("Multi_Cancer_Survival_Analysis.R")  # Source this first as it contains base functions
+source("High_SRRM3_PSI_Analysis.R")        # Source this second as it depends on the first
 
 # Run analysis
 tryCatch({
